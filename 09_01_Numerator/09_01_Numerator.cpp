b06e908c2a6a7c2feb987a241bd7645ce1fccc3b
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-01-24 12:25:18 +0300

    Add 9 work

---
+++ 09_01_Numerator/09_01_Numerator.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	bool operator==(const Fraction& fraction) const
+	{
+		return (numerator_ == fraction.numerator_) && (denominator_ == fraction.denominator_);
+	}
+	bool operator!=(const Fraction& fraction) const = default;			// через default получилось только здесь? Почему не получается с остальными не пойму(((((?
+	bool operator<(const Fraction& fraction) const
+	{
+		return (static_cast<double>(numerator_ / denominator_) < static_cast<double>(fraction.numerator_ / fraction.denominator_));
+	}
+	bool operator>(const Fraction& fraction) const
+	{
+		return (static_cast<double>(numerator_ / denominator_) > static_cast<double>(fraction.numerator_ / fraction.denominator_));
+	}
+	bool operator<=(const Fraction& fraction) const
+	{
+		return (static_cast<double>(numerator_ / denominator_) <= static_cast<double>(fraction.numerator_ / fraction.denominator_));
+	}
+	bool operator>=(const Fraction& fraction) const
+	{
+		return (static_cast<double>(numerator_ / denominator_) >= static_cast<double>(fraction.numerator_ / fraction.denominator_));
+	}
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
