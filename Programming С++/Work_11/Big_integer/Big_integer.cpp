511cf8c29e7c38dee0f622609df70df216294aa3
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-04-20 18:57:13 +0300

    Add 11 Work

---
+++ Programming С++/Work_11/Big_integer/Big_integer.cpp
@@ -0,0 +1,91 @@
+﻿#include <memory>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class BigInteger {
+private:
+    vector<unsigned int> digit;         //вектор положительного числа из чисел. со знаком большого числа заморачиваться не буду)
+public:
+    BigInteger() {};                                                        //пустой конструктор
+    BigInteger(string str) {
+        for (auto it{ str.begin() }; str.end() != it; it++) {
+            digit.push_back(((unsigned int)*it) - ((unsigned int)'0'));   //переводим в vector
+        }
+        reverse(digit.begin(), digit.end());
+    }
+
+   BigInteger(const BigInteger& copy){
+       cout << "Конструктор копирования\n";
+       digit = copy.digit;
+   }
+
+   BigInteger(BigInteger&& moved) noexcept {     //конструктор перемещения
+       this->digit = move(moved.digit);
+       cout << "Конструктор перемещения\n";
+   }
+
+   BigInteger& operator = (BigInteger&& moved) noexcept { //перемещающий оператор
+       cout << "Оператор перемещения\n";
+       *this = move(moved);
+       return *this;
+   }
+
+   BigInteger& operator = (BigInteger& copy) {      //копирующий оператор сделал как перемещающийся
+       cout << "Оператор копирования\n";            //не получается вызвать оператор перемещения((((
+       this->digit = move(copy.digit);              //Неужели постоянно надо использовать move для
+       return *this;                                //преобоазования lvalue в rvalue?
+   }
+
+   BigInteger operator+ (const BigInteger& a) {
+       unsigned int flag{};
+       unsigned int tmp{};
+       BigInteger temp;
+       for (size_t i{ }; i < (digit.size() > a.digit.size() ? digit.size() : a.digit.size()); i++){
+           if (i >= digit.size())
+               tmp = flag + 0 + a.digit[i];
+           else if (i >= a.digit.size())
+               tmp = flag + digit[i] + 0;
+           else
+               tmp = flag + digit[i] + a.digit[i];
+
+           flag = tmp / 10;
+           tmp = tmp % 10;
+           temp.digit.push_back(tmp);
+       }
+       if (flag != 0)temp.digit.push_back(flag);
+       return temp;
+   }
+
+   vector<unsigned int> getVec() const {
+       return digit;
+   }
+
+   ~BigInteger() {}
+};
+
+ostream& operator<<(ostream& strm, const BigInteger& a) {
+    auto vec = a.getVec();
+    reverse(vec.begin(), vec.end());
+    string str;
+    for (auto value : vec) {
+        str += to_string(value);
+    }
+    strm << str;
+    return strm;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    BigInteger a("114575");
+    BigInteger b{ "78524" };
+    auto c = a + b;
+    cout << c << endl;
+    return 0;
+  
+}
+
---
+++ Programming С++/Work_11/moveVector/moveVector.cpp
@@ -0,0 +1,20 @@
+﻿#include <memory>
+#include <iostream>
+#include <vector>
+
+template<class T>
+void move_vectors(std::vector<T>& one, std::vector<T>& two) {
+	two = std::move(one);
+//	two = one;
+}
+
+int main()
+{
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+	move_vectors(one, two);
+	for (auto v : two) std::cout << " " << v;
+	for (auto v : one) std::cout << " " << v;		//С Move все работает!, а без просто копирование
+	return 0;
+}
+
