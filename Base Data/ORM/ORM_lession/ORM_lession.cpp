b71d0b1168d10eeac5d7936f0b29849da28c4653
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-05-29 22:54:57 +0300

    Add dorabotka pq++

---
+++ Base Data/ORM/ORM_lession/ORM_lession.cpp
@@ -0,0 +1,64 @@
+﻿
+#include <iostream>
+#include <Wt/Dbo/Dbo.h>
+#include <Wt/Dbo/backend/Postgres.h>
+#include <string>
+
+using namespace std;
+
+class Book;		//обьявим класс Book
+//---------------------------------------
+class Publisher
+{
+public:
+	string name{ "" };
+	int id;
+	Wt::Dbo::collection<Wt::Dbo::ptr<Book>> books;
+	template <class A>
+	void persist(A& a) {
+		Wt::Dbo::field(a, id, "id");
+		Wt::Dbo::field(a, name, "name");
+		Wt::Dbo::hasMany(a, books, Wt::Dbo::ManyToOne, "id_publisher");
+	};
+};
+//-------------------------------------------------
+class Book
+{
+public:
+	int id;
+	string title{ "" };
+	Wt::Dbo::ptr<Publisher> id_publisher;
+	template<class A>
+	void persist(A& a) {
+		Wt::Dbo::field(a, id, "id");
+		Wt::Dbo::field(a, title, "title");
+		Wt::Dbo::belongsTo(a, id_publisher, "id_publisher");
+	}
+};
+
+
+int main()
+{
+	try
+	{
+			string ConnectionString =
+			"host=localhost "
+			"port=5432 "
+			"dbname=book "
+			"user=postgres "
+			"password=80017";
+
+			/*Wt::Dbo::backend::Postgres*/ auto postgres{ make_unique<Wt::Dbo::backend::Postgres>(ConnectionString) };
+			Wt::Dbo::Session session;
+			session.setConnection(move(postgres));
+
+			session.mapClass<Publisher>("publisher");	//создаем таблицу publisher
+			session.mapClass<Book>("book");				//table book
+
+	}
+	catch (const Wt::Dbo::Exception& e)
+	{
+		cout << e.what();
+	}
+    std::cout << "Hello World!\n";
+}
--- Base Data/main.cpp
+++ Base Data/main.cpp
@@ -37,29 +37,31 @@ explicit TransactionBase(const string host, const string port, const string dbna
 	//созданеие базы
 	void TransactionCreate() {
 		pqxx::work creat_table{ *p_conn };
-		creat_table.exec("create table if not exists Phones( "
+		creat_table.exec("create table if not exists Client( "
+			"Id_client integer primary key, "
 			"Name text not null, "
 			"Sur_name text not null, "
-			"Number text, "
-			"E_mail text primary key not null);");
+			"E_mail text not null);");
+		//creat_table.commit();
+		creat_table.exec("create table if not exists Phone( "
+			"id_phone text primary key, "
+			"id_client integer references Client(Id_client), "
+			"Number text);");
 		creat_table.commit();
 	};
 
 	//добавление нового клиента
-	void AddClient(const string& name, const string& surname, const string& email, const string& phonenumber = "") {
+	void AddClient(string id_c, const string& name, const string& surname, const string& email) {
 		pqxx::work creat_client{ *p_conn };
-		if (phonenumber != "") creat_client.exec("insert into Phones (Name, Sur_name, E_mail, Number) values ('" + creat_client.esc(name) + "','" + creat_client.esc(surname) +
-			"','" + creat_client.esc(email) + "','" + creat_client.esc(phonenumber) + "');");
-		else
-			creat_client.exec("insert into Phones (Name, Sur_name, E_mail) values ('" + creat_client.esc(name) + "','" + creat_client.esc(surname) +
+		creat_client.exec("insert into Client (Id_client, Name, Sur_name, E_mail) values ('"+id_c+"','" + creat_client.esc(name) + "','" + creat_client.esc(surname) +
 				"','" + creat_client.esc(email) + "');");
 		creat_client.commit();
 	};
 
 	//добавляем номер телефона к существующему клиенту
-	void AddTellNumber() {
+	void AddTellNumber(const string& id_p, const string& id_c, const string& phonenumber) {
 		pqxx::work add_tell{ *p_conn };
-		add_tell.exec("update Phones set Number='555555' where E_mail='email@5';");
+		add_tell.exec("insert into Phone (id_phone, id_client, Number) values ('" + id_p + "','" + add_tell.esc(id_c) + "','" + add_tell.esc(phonenumber) + "');");
 		add_tell.commit();
 	};
 
@@ -102,13 +104,23 @@ int main() {
 	try
 	{
 		tb.TransactionCreate(); //create
-		tb.AddClient("Vasy", "Pupkin", "email@1", "1234567");
-		tb.AddClient("Pety", "Ivanov", "email@3", "18768");
+		//----------add client-----------------------------
+		tb.AddClient("2", "aaaa", "aaaaa", "email@4");
+		tb.AddClient("3", "Vasy", "Pupkin", "email@1");
+		tb.AddClient("4", "Pety", "Ivanov", "email@3");
+		tb.AddClient("5", "Pety", "Ivanov", "email@3");
+		//---------add phone------------------------------
+		tb.AddTellNumber("1", "2", "1234567");
+		tb.AddTellNumber("2", "2", "123456789"); //у ааааа два телефона
+		tb.AddTellNumber("3", "3", "234234234"); //а это Вася 1 телефон
+
+		/*tb.AddClient("Pety", "Ivanov", "email@3", "18768");
 		tb.AddClient("Pety", "Ivanov", "email@5");
-		tb.AddTellNumber();
+		tb.AddClient("Pety", "Ivanov", "email@3", "18769");*/
+		/*tb.AddTellNumber();
 		tb.DelTell("email@5");
 		tb.DelClt("email@3");
-		tb.Select("Vasy");
+		tb.Select("Vasy");*/
 	}
 	catch (const pqxx::sql_error& e)
 	{
