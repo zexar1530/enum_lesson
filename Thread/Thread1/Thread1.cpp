99b4f53c71242b88e0773fdea403f5bcaf48999a
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-06-18 21:29:42 +0300

    Atomic ADD

--- Thread/Thread1/Thread1.cpp
+++ Thread/Thread1/Thread1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <chrono>
 #include <thread>
 #include <iostream>
+#include <atomic>
 
 using namespace std;
 
@@ -10,22 +11,24 @@ public:
 	Turn(unsigned int);
 	~Turn();
 	void countClient() {
+		//this_thread::sleep_for(2000ms);
 		for (int i{}; i < 10; i++) {		// 10 Clients +
-			this_thread::sleep_for(1000ms);
-			count++;
+			this_thread::sleep_for(100ms);
+			count.operator++();
 			cout << "Turn of client +1 " << count << endl;
 		}
 	};
 	void countOperator() { 
+		//this_thread::sleep_for(2000ms);
 		for (int i{}; i < 10; i++) {	
-			this_thread::sleep_for(2000ms);
-			count -= 2;
+			this_thread::sleep_for(200ms);
+			count.operator-=(2);
 			cout << "Turn of client -2 " << count << endl;
 		} 
 	};
 
 private:
-	unsigned int count{};
+	atomic <unsigned> count{};
 };
 
 Turn::Turn(unsigned int count) :count (count)  {};
@@ -33,11 +36,10 @@ Turn::~Turn() {};
 
 int main()
 {
-	Turn turn{ 20 };  //20 уже в очереди
+	Turn turn(20);  //20 уже в очереди
 		thread t1(&Turn::countClient, ref(turn));
 		thread t2([&turn]() {turn.countOperator(); }); //попробовал лямбду
 		t1.join();
 		t2.join();
-    cout << "Hello World!\n";
 	return 0;
 }
