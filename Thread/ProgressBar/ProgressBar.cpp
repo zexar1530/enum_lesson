92651e7e57ee79f4281dd82e864316b3b4a3802f
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-06-21 15:39:14 +0300

    Add SWAP thread

--- Thread/ProgressBar/ProgressBar.cpp
+++ Thread/ProgressBar/ProgressBar.cpp
@@ -7,23 +7,24 @@
 
 using namespace std;
 
-mutex m;
+mutex m1, m2;
 
 void ProgressBar(int row) {
-    m.lock();
+    m1.lock();
     cout << row << "\t" << this_thread::get_id() << "\t" << endl;
-    m.unlock();
+    m1.unlock();
     auto start = chrono::high_resolution_clock::now();
     for (int i{}; i < 12; i++) {
-        this_thread::sleep_for(200ms);
-        m.lock();
+        this_thread::sleep_for(500ms);
+        m2.lock();
+        this_thread::sleep_for(300ms);
         consol_parameter::SetPosition(i + 16, row + 1);
         consol_parameter::SetColor(0, 1);
         cout << " ";
-        m.unlock();
+        m2.unlock();
     }
     auto fin = chrono::high_resolution_clock::now();
-    lock_guard<mutex> l(m);
+    lock_guard<mutex> l(m2);
     consol_parameter::SetColor(15, 0);
     consol_parameter::SetPosition(30, row + 1);
     cout << chrono::duration_cast<chrono::duration<double>>(fin - start).count() * 1000 << "ms";
---
+++ Thread/SWAP_DATA_THREAD/SWAP_DATA_THREAD.cpp
@@ -0,0 +1,68 @@
+﻿
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+class Data {
+public:
+    Data(int);
+    Data() = default;
+    Data(const Data&) = delete;
+    Data(Data&&) = delete;
+
+    int number{};
+    mutex m;
+};
+
+Data::Data(int i) :number(i) {};
+
+void FuncSwapLock(Data&, Data&);
+void FuncSwapScoped(Data&, Data&);
+void FuncSwapUnique(Data&, Data&);
+
+int main()
+{
+    Data d1(12345);
+    Data d2(67890);
+    thread t1(&FuncSwapLock, ref(d1), ref(d2));
+    thread t2(&FuncSwapScoped, ref(d1), ref(d2));
+    thread t3(&FuncSwapUnique, ref(d1), ref(d2));
+    t1.join();
+    t2.join();
+    t3.join();
+    return 0;
+}
+
+void FuncSwapLock(Data& data1, Data& data2) {
+    data1.m.lock();
+    data2.m.lock();
+    int tmp = data1.number;
+    data1.number = data2.number;
+    data2.number = tmp;
+    cout << "Data1 = " << data1.number << "\t" << "Data2 = " << data2.number << endl;
+    data1.m.unlock();
+    data2.m.unlock();
+};
+
+void FuncSwapScoped(Data& data1, Data& data2) {
+    scoped_lock l(data1.m, data2.m);
+    int tmp = data1.number;
+    data1.number = data2.number;
+    data2.number = tmp;
+    cout << "Data1 = " << data1.number << "\t" << "Data2 = " << data2.number << endl;
+}
+
+void FuncSwapUnique(Data& data1, Data& data2) {
+    unique_lock l1(data1.m, defer_lock);
+    unique_lock l2(data2.m, defer_lock);
+    l1.lock();
+    l2.lock();
+    int tmp = data1.number;
+    data1.number = data2.number;
+    data2.number = tmp;
+    cout << "Data1 = " << data1.number << "\t" << "Data2 = " << data2.number << endl;
+    l1.unlock();
+    l2.unlock();
+}
