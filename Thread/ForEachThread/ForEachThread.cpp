999745673942e93c2c4f74f049e879122bcd2c6c
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-06-23 20:27:24 +0300

    add Future Async

---
+++ Thread/ForEachThread/ForEachThread.cpp
@@ -0,0 +1,49 @@
+﻿#include <vector>
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+void f(int& data) {
+	data += 1;
+}
+
+template<class Iter, class Func>
+void ParallelForEach(Iter first, Iter last, Func f) {
+
+	unsigned long long length = distance(first, last);
+
+	if (!length)
+		return;
+
+	if (length < 2) {
+		for_each(first, last, f);
+	}
+	else {
+		Iter mid = first + length / 2;
+		future<void> result = async(&ParallelForEach<Iter, Func>, first, mid, f);
+		ParallelForEach(mid, last, f);
+		result.get();
+	}
+
+}
+int main()
+{
+    //------------заполняем вектор-------------------------
+    vector<int> v(11, 2);
+    for (auto& i : v)
+        cout << i << " ";
+    cout << "\n\n";
+
+    ParallelForEach(v.begin(), v.end(), f);
+
+    for (auto& i : v)
+        cout << i << " ";
+    cout << "\n\n";
+
+    return 0;
+}
+
---
+++ Thread/Vec_Future/Vec_Future.cpp
@@ -0,0 +1,49 @@
+﻿#include <future>
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+
+//----------Create vector and fill. Print this------------------
+    vector<int> v;
+    for (int i{}; i < 10; i++) {
+        v.push_back(rand() % 100);
+    }
+    for (auto& v : v)
+        cout << v << " ";
+    cout << "\n\n";
+
+    auto Lymda_Future = [&v](int i, promise<int> p_int) {
+        int min = i;
+        for (int j = i + 1; j < v.size(); j++) {
+            if (v[j] < v[min]) {
+                min = j;
+            }
+        }
+        p_int.set_value(min);   //возращаем минимальный индекс
+        };
+
+//-------------Запускаем основной цикл поиска------------------
+    for (int i{}; i < v.size() - 1; i++) {
+        promise<int> p_int;
+        future<int> future_result = p_int.get_future();
+        async(launch::async, Lymda_Future, i, std::move(p_int));
+        int min = future_result.get();
+        if (i != min)
+            swap(v[i], v[min]);         //если индексы не равны то меняем
+    }
+
+//---------------Печатаем что получилось------------------
+    for (auto& v : v)
+        cout << v << " ";
+    cout << "\n";
+
+    return 0;
+}
+
