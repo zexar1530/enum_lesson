2031fc2be80577bd5c0b4ad1849e186911180d32
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-07-01 20:20:47 +0300

    add thread_pool

--- Thread/ForEachThread/ForEachThread.cpp
+++ Thread/ForEachThread/ForEachThread.cpp
@@ -5,70 +5,102 @@
 #include <string>
 #include <thread>
 
-using namespace std;
+//using namespace std;
 
-//void f(int& data) {
-//	data += 1;
+////void f(int& data) {
+////	data += 1;
+////}
+////
+////template<class Iter, class Func>
+////void ParallelForEach(Iter first, Iter last, Func f) {
+////
+////	unsigned long long length = distance(first, last);
+////
+////	if (!length)
+////		return;
+////
+////	if (length < 2) {
+////		for_each(first, last, f);
+////	}
+////	else {
+////		Iter mid = first + length / 2;
+////		future<void> result = async(&ParallelForEach<Iter, Func>, first, mid, f);
+////		ParallelForEach(mid, last, f);
+////		result.get();
+////	}
+////
+////}
+//void f(vector<int>::iterator& i) {
+//    *i += 2;
+//    i++;
 //}
 //
-//template<class Iter, class Func>
-//void ParallelForEach(Iter first, Iter last, Func f) {
+////template<class Iter,class Func>
+////void ForE(Iter begin, Iter end, Func f) {
+////    while (begin!=end)
+////    {
+////        f(begin);
+////    }
+////}
 //
-//	unsigned long long length = distance(first, last);
 //
-//	if (!length)
-//		return;
-//
-//	if (length < 2) {
-//		for_each(first, last, f);
-//	}
-//	else {
-//		Iter mid = first + length / 2;
-//		future<void> result = async(&ParallelForEach<Iter, Func>, first, mid, f);
-//		ParallelForEach(mid, last, f);
-//		result.get();
-//	}
-//
+//template<class Iter>
+//void ForE(Iter begin, Iter end, void (*FUN)(vector<int>::iterator, vector<int>::iterator)){
+//    FUN(begin, end);
 //}
-void f(vector<int>::iterator& i) {
-    *i += 2;
-    i++;
+//
+//int main()
+//{
+//    //------------заполняем вектор-------------------------
+//    vector<int> v(11, 2);
+//    for (auto& i : v)
+//        cout << i << " ";
+//    cout << "\n\n";
+//
+//    auto Lymda = [](vector<int>::iterator begin, vector<int>::iterator end) {
+//        while (begin != end)
+//            f(begin);
+//        };
+//
+//    //ParallelForEach(v.begin(), v.end(), f);
+//    ForE(v.begin(), v.end(), Lymda);
+//
+//    for (auto& i : v)
+//        cout << i << " ";
+//    cout << "\n\n";
+//
+//    return 0;
+//}
+
+std::vector<int> data;
+std::condition_variable data_cond;
+std::mutex m;
+
+void thread_func1()
+{
+	std::cout << "000000000\n";
+	std::unique_lock <std::mutex> lock(m);
+	data.push_back(10);
+	data_cond.notify_one();
+	data.push_back(20);
+	std::cout << "111111111\n";
 }
 
-//template<class Iter,class Func>
-//void ForE(Iter begin, Iter end, Func f) {
-//    while (begin!=end)
-//    {
-//        f(begin);
-//    }
-//}
-
-
-template<class Iter>
-void ForE(Iter begin, Iter end, void (*FUN)(vector<int>::iterator, vector<int>::iterator)){
-    FUN(begin, end);
+void thread_func2()
+{
+	std::cout << "22222222222\n";
+	std::unique_lock<std::mutex> lock(m);
+	data_cond.wait(lock, false/*[] {
+		return !data.empty();
+		}*/);
+	std::cout << data.back() << std::endl;
+	std::cout << data.front() << std::endl;
 }
 
 int main()
 {
-    //------------заполняем вектор-------------------------
-    vector<int> v(11, 2);
-    for (auto& i : v)
-        cout << i << " ";
-    cout << "\n\n";
-
-    auto Lymda = [](vector<int>::iterator begin, vector<int>::iterator end) {
-        while (begin != end)
-            f(begin);
-        };
-
-    //ParallelForEach(v.begin(), v.end(), f);
-    ForE(v.begin(), v.end(), Lymda);
-
-    for (auto& i : v)
-        cout << i << " ";
-    cout << "\n\n";
-
-    return 0;
+	std::thread th1(thread_func1);
+	std::thread th2(thread_func2);
+	th1.join();
+	th2.join();
 }
-
--- Thread/Thread_pool/Thread_pool.cpp
+++ Thread/Thread_pool/Thread_pool.cpp
@@ -3,52 +3,121 @@
 #include <functional>
 #include <queue>
 #include <vector>
+#include <mutex>
+#include <atomic>
 
 using namespace std;
 
-class thread_pool {
-    std::atomic_bool done;
-    thread_safe_queue<std::function<void()> > work_queue;
-    std::vector<std::thread> threads;
-    void worker_thread() {
-        while (!done) {
-            function<void()> task;
-            if (work_queue.try_pop(task)) {
-                    task();
-            }
-            else {
-                this_thread::yield();
-            }
+static inline void f1() {
+    this_thread::sleep_for(100ms);
+    for (unsigned int i{}; i < 10000; i++)
+        i += 100;
+    cout << "Function 1 ";
+}
+
+static inline void f2() {
+    this_thread::sleep_for(200ms);
+    for (unsigned int i{}; i < 10000; i++)
+        i += 100;
+
+    cout << "Function 2 ";
+}
+
+template<class Func>    // класс потокобезопасной очереди
+class safe_queue {
+public:
+    safe_queue() = default; //конструктор и деструктор по умолчанию
+    ~safe_queue() = default;
+
+    void push(Func f) {
+        unique_lock<mutex> l(m);
+        q.push(f);
+    }
+
+    void front() {
+        unique_lock<mutex> l(m);
+        if (!q.empty()) {
+            q.front()();
+            q.pop();
         }
     }
 
+    bool empty() {
+        unique_lock<mutex> l(m);
+        return q.empty();
+    }
+
+private:
+    mutex m;
+    queue<Func> q;
+};
+
+class thread_pool {
 public:
-    thread_pool() : done(false) {
-        unsigned const thread_count = std::thread::hardware_concurrency();
+    thread_pool() : done(false) {           //конструктор пула
+        unsigned int const thread_count = thread::hardware_concurrency()-3; //отнимем 3 на всякий))))
         try {
             for (unsigned i = 0; i < thread_count; ++i) {
-                threads.push_back(thread(&thread_pool::worker_thread, this));
+                threads.push_back(thread(&thread_pool::work, this)); //создаем потоки из work
             }
         }
         catch (...) {
-            done = true;
+            done = true; //если что то не так то тормозим work() и передаем исключение дальше
             throw;
         }
     }
 
     ~thread_pool() {
-        done = true;
+        done = true;    //при разрушении обьекта тормозим work()
     }
 
-    template<class Function>
-    void submit(Function f) {
-        work_queue.push(std::function<void()>(f));
+    template<class Func>        //принимаем функцию и отправляем в очередь
+    void submit(Func f) {
+        work_queue.push(function<void()> (f));
     }
 
+    void poolJoin() {
+        for (auto& t : threads)
+            t.join();
+    }
+
+private:
+    mutex m;
+    atomic_bool done;       //flag
+    safe_queue<function<void()>> work_queue;  //наша очередь
+    vector<thread> threads; //наши потоки
+
+    void work() {
+        /*while (!done) {*/             //Посоветуйте как выйти из цикла потоков
+        for (int i{}; i < 10; i++) {
+            if (!work_queue.empty()) {  //если очередь не пуста то работаем
+                work_queue.front();     //заспускаем функцию
+                unique_lock<mutex> l(m);
+                cout << "\tThread " << this_thread::get_id() << endl;
+            }
+            else {
+                this_thread::yield();   //если пуста то пусть поток покурит)))))
+            }
+        }
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    {
+        thread_pool pool;   //создали наш обьект и потоки
+        pool.submit(f1);
+        pool.submit(f2);
+        pool.submit(f1);
+        pool.submit(f2);
+        pool.submit(f1);
+        pool.submit(f2);
+        pool.submit(f1);
+        pool.submit(f2);
+        pool.poolJoin();
+    }
+    this_thread::sleep_for(5000ms);
+    cout << "end!!!!!!!!\n";
+    return 0;
 }
 
