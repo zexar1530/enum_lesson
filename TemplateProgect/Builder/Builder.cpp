d5e95aac126697666ca1cb393ffe4500672f31dc
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-07-24 19:45:40 +0300

    Task 2 Tamplate bilder

--- TemplateProgect/Builder/Builder.cpp
+++ TemplateProgect/Builder/Builder.cpp
@@ -5,11 +5,17 @@
 int main()
 {
     SqlSelectQueryBuilder query_builder;
-    query_builder.AddColum("name").AddColum("phone");
+    //query_builder.AddColum("name").AddColum("phone");
+    query_builder.AddColumn({ "name", "phone" });
     query_builder.AddFrom("students");
-    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+    //query_builder.AddWhere("id", "42").AddWhere("name", "John");
+    map<string, string> where_map = {{"id", "42"}, {"name", "John"}};
+    query_builder.AddWhere(where_map);
 
-    assert(query_builder.BuildQuery() == "SELECT name, phone FROM students WHERE id=42 AND name=John; ");
+    string query{ query_builder.BuildQuery() };
+
+    assert(query == "SELECT name, phone FROM students WHERE id=42 AND name=John; ");
+    cout << query_builder.BuildQuery();
 
     return 0;
 }
--- TemplateProgect/Builder/SqlSelectQueryBuilder.h
+++ TemplateProgect/Builder/SqlSelectQueryBuilder.h
@@ -16,18 +16,18 @@ struct BaseData {
 class SqlSelectQueryBuilder
 {
 private:
-	int count{};	//количество записей;
+	//int count{};	//количество записей;
 	BaseData base;
 
 public:
-	SqlSelectQueryBuilder() {				//при создании обьекта добавляем *
-		base.name_colum.push_back("*");	
-	}
+	//SqlSelectQueryBuilder() {				//при создании обьекта добавляем *
+	//	base.name_colum.push_back("*");	
+	//}
 
 	SqlSelectQueryBuilder& AddColum(const string& name_colum) {
-		if (!count)
-			base.name_colum.pop_back();	//если еще пусто то выбрасываем *
-		count++;	//увеличиваем количество
+		//if (!count)
+		//	base.name_colum.pop_back();	//если еще пусто то выбрасываем *
+		//count++;	//увеличиваем количество
 		base.name_colum.push_back(name_colum); //Добавляем имя колонки
 		return *this;	//и возращаем себя
 	}
@@ -40,7 +40,7 @@ public:
 	SqlSelectQueryBuilder& AddWhere(const string& name_colum, const string& date) {
 		base.where_colum.push_back(name_colum);
 		base.date_colum.push_back(date);
-		count++;
+		//count++;
 		return *this;
 	}
 
@@ -48,18 +48,38 @@ public:
 		string query;
 		string res_select, res_where;
 		std::string result = "";
-		for (int i{}; i < base.name_colum.size(); i++) { // собираем Select
-			if (i == base.name_colum.size() - 1)
-				res_select = res_select + base.name_colum[i];
-			else res_select = res_select + base.name_colum[i] + ", ";
+
+		if (!base.name_colum.empty()) {
+			for (int i{}; i < base.name_colum.size(); i++) { // собираем Select
+				if (i == base.name_colum.size() - 1)
+					res_select = res_select + base.name_colum[i];
+				else res_select = res_select + base.name_colum[i] + ", ";
+			}
 		}
+		else res_select = "*";
+
 		for (int i{}; i < base.where_colum.size(); i++) { //собираем Where
 			if (i == base.where_colum.size() - 1)
 				res_where = res_where + base.where_colum[i] + "=" + base.date_colum[i];
 			else res_where = res_where + base.where_colum[i] + "=" + base.date_colum[i] + " AND ";
 		}
+
 		query = "SELECT " + res_select + " FROM " + base.name_table + " WHERE " + res_where + "; ";
 		return query;	//возращаем запрос
+	}
+
+	SqlSelectQueryBuilder& AddWhere(const std::map<std::string, std::string>& key_val) noexcept {
+		for (const auto& kv : key_val) {
+			base.where_colum.push_back(kv.first);
+			base.date_colum.push_back(kv.second);
+		}
+		return *this;
+	}
+
+	SqlSelectQueryBuilder& AddColumn(const std::vector<std::string>& columns) noexcept {
+		for (const auto& column : columns)
+			base.name_colum.push_back(column);
+		return *this;
 	}
 };
 
