5ae262602ba0ad709d8d3a48e2f5b4158e416a50
Andrey Zakharenkov <zexar1530@yandex.ru>
2024-07-26 13:37:35 +0300

    Add task 2

---
+++ TemplateProgect/Decorator/Decorator.cpp
@@ -0,0 +1,88 @@
+﻿#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class Paragraph :public DecoratedText {
+public:
+    Paragraph(Text* text) :DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed :DecoratedText {
+public:
+    Reversed(Text* text) :DecoratedText(text) {}
+    void render(const std::string& data) const override {
+        std::string d_cout{ data }; //с const пришлось повозится)))). Не сразу понял в чем подвох
+        std::reverse(d_cout.begin(),d_cout.end());
+        std::cout << d_cout;
+    }
+};
+
+class Link :DecoratedText {
+public:
+    Link(Text* text) :DecoratedText(text) {}
+    void render(const std::string& addr, const std::string& data) const { //перегрузили
+        std::cout << "<a href=" << addr << ">" << data << "</a>";
+    }
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        /*Text::render(data);*/
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+int main() {
+    auto text_block = std::make_shared<BoldText>(new ItalicText(new Text()));
+    text_block->render("Hello world");
+
+    std::cout << std::endl;
+
+    auto text_block1 = new Paragraph(new Text());
+    text_block1->render("Hello world");
+
+    std::cout << std::endl;
+
+    auto text_block2 = new Reversed(new Text());
+    text_block2->render("Hello world");
+
+    std::cout << std::endl;
+
+    auto text_block3 = new Link(new Text());
+    text_block3->render("netology.ru", "Hello world");
+};
---
+++ TemplateProgect/proxy_database/proxy_database.cpp
@@ -0,0 +1,71 @@
+﻿
+#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class OneShotDB : VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1) :real_db_(real_object), count_read_(shots) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (!count_read_)
+            return "ERROR";
+        count_read_--;
+        return VeryHeavyDatabase::GetData(key);
+    }
+private:
+    size_t count_read_{};
+    VeryHeavyDatabase* real_db_;
+    std::map<std::string, std::string> cache_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+
+int main() {
+    /*auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << test_db.GetData("key") << std::endl;*/
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+    return 0;
+}
